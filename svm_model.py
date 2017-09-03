460c17e9f974989561757043366124ee55973891
jehung <jehung@me.com>
2017-09-03 10:10:11 -0700

    Add parallel computing capability

--- svm_model.py
+++ svm_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
-from sklearn.svm import SVR
+from sklearn.svm import SVC
 from imblearn.over_sampling import SMOTE, ADASYN
 from imblearn.combine import SMOTEENN
 from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
@@ -12,40 +12,43 @@ from sklearn.preprocessing import label_binarize
 from sklearn.preprocessing import scale
 from sklearn.model_selection import cross_val_predict, train_test_split
 from joblib import Parallel, delayed
+import multiprocessing
 pd.set_option('display.max_columns', None)
 import get_all_data
 import utility
 
 
 
-def train_size_svm(X, y):
+def train_size_svc(X, y, size):
     d = {'train': [], 'cv set': [], 'test': []}
     training_features, test_features, \
     training_target, test_target, = train_test_split(X, y, test_size=0.2, random_state=12)
-    for size in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-        print('size', size)
-        X_train, X_val, y_train, y_val = train_test_split(training_features, training_target, train_size=size)
-        smote = SMOTE(ratio=1)
-        X_train_res, y_train_res = smote.fit_sample(X_train, y_train)
+    #for size in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+    print('size', size)
+    print('here')
+    X_train, X_val, y_train, y_val = train_test_split(training_features, training_target, train_size=size)
+    smote = SMOTE(ratio=1)
+    X_train_res, y_train_res = smote.fit_sample(X_train, y_train)
+    print('start')
+    clf = SVC(verbose=True)
+    clf.fit(X_train_res, y_train_res)
+    print('process')
+    d['train'].append(f1_score(y_train_res, clf.predict(X_train_res), average='weighted'))
+    d['cv set'].append(f1_score(y_val, clf.predict(X_val), average='weighted'))
+    d['test'].append(f1_score(test_target, clf.predict(test_features), average='weighted'))
 
-        clf = SVR()
-        clf.fit(X_train_res, y_train_res)
-
-        d['train'].append(f1_score(y_train_res, clf.predict(X_train_res), average='weighted'))
-        d['cv set'].append(f1_score(y_val, clf.predict(X_val), average='weighted'))
-        d['test'].append(f1_score(test_target, clf.predict(test_features), average='weighted'))
-
+    print('end')
     return d
 
 
 
-def complexity_svm(X, y):
+def complexity_svc(X, y):
     #X_train, y_train, X_test, y_test = train_test_split(X,y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=778)
     smote = SMOTE(ratio=1)
     X_train_res, y_train_res = smote.fit_sample(X_train, y_train)
     print('Start Search')
-    svm = SVR()
+    svc= SVC()
     pipe = Pipeline([('smote', smote), ('svm', svm)])
     param_grid = {
         'svm__kernel': ('rbf', 'sigmoid'),
@@ -53,7 +56,7 @@ def complexity_svm(X, y):
         'svm__gamma': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
     }
 
-    grid_search = GridSearchCV(estimator=pipe, param_grid=param_grid, n_jobs=6, cv=10, scoring='neg_log_loss',verbose=5)
+    grid_search = GridSearchCV(estimator=pipe, param_grid=param_grid, n_jobs=6, cv=10, scoring='neg_log_loss')
     grid_search.fit(X_train_res, y_train_res)
     clf = grid_search.best_estimator_
     print('clf', clf)
@@ -75,6 +78,9 @@ def complexity_svm(X, y):
 if  __name__== '__main__':
     all_data = get_all_data.get_all_data()
     train, target = get_all_data.process_data(all_data)
-    df = train_size_svm(train, target)
-    #clf, score, mat = complexity_svm(train, target)
+    num_cores = multiprocessing.cpu_count()
+    results = Parallel(n_jobs=num_cores)(delayed(train_size_svc)(train, target, size) for size in [0.4, 0.5])
+    df = train_size_svc(train, target)
+    print(df)
+    clf, score, mat = complexity_svc(train, target)
     print(df)
