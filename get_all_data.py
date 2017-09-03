6c7be7b8bdf1d598ad40d669f54e56d4debf4033
jehung <jehung@me.com>
2017-09-03 01:52:13 -0700

    Update get_all_data.py for new functions

--- get_all_data.py
+++ get_all_data.py
@@ -36,7 +36,7 @@ def get_all_data():
               'sample_svcg_2010.txt', 'sample_svcg_2009.txt', 'sample_svcg_2008.txt', 'sample_svcg_2008.txt']
 
     merged_data = pd.DataFrame()
-    for i in range(1,2):
+    for i in [0]:
         print(files[i])
         raw = pd.read_csv(dir + files[i], sep='|', header=None, low_memory=False)
         raw.columns = ['credit_score', 'first_pmt_date', 'first_time', 'mat_date', 'msa', 'mi_perc', 'units',
@@ -54,44 +54,47 @@ def get_all_data():
 
         merged_data = merged_data.append(data)
 
-    data.drop(['seller_name', 'servicer_name', 'first_pmt_date', 'mat_date', 'msa'], axis=1, inplace=True)
+    merged_data.drop(['seller_name', 'servicer_name', 'first_pmt_date', 'mat_date', 'msa', 'net_proceeds'], axis=1,
+                     inplace=True)
 
     # all data must have the following: credit_score, ocltv, odti, oltv, oint_rate, curr_upb
     # remove any datapoints with missing values from the above features
-    data.dropna(subset=['credit_score', 'odti', 'oltv', 'oint_rate', 'curr_upb'], how='any', inplace=True)
-    data.credit_score = pd.to_numeric(data['credit_score'], errors='coerce')
-    data.yearmon = pd.to_datetime(data['yearmon'], format='%Y%m')
-    data.fillna(value=0, inplace=True, axis=1)
+    merged_data.dropna(subset=['credit_score', 'odti', 'oltv', 'oint_rate', 'curr_upb'], how='any', inplace=True)
+    merged_data.credit_score = pd.to_numeric(data['credit_score'], errors='coerce')
+    merged_data.yearmon = pd.to_datetime(data['yearmon'], format='%Y%m')
+    merged_data.fillna(value=0, inplace=True, axis=1)
+
+    merged_data.sort_values(['loan_num'], ascending=True).groupby(['yearmon'],
+                                                                  as_index=False)  ##consider move this into the next func
+    merged_data.set_index(['loan_num', 'yearmon'], inplace=True)  ## consider move this into the next func
 
     return merged_data
 
 
-def process_data_rev(data):
-    #data.sort_values(['loan_num'], ascending=True).groupby(['yearmon'], as_index=False)  ##consider move this out
-    #data.set_index(['loan_num', 'yearmon'], inplace=True) ## consider move this out
+def process_data(data):
+    # data.sort_values(['loan_num'], ascending=True).groupby(['yearmon'], as_index=False)  ##consider move this out
+    # data.set_index(['loan_num', 'yearmon'], inplace=True) ## consider move this out
     y = data['curr_delinq']
-    #data['prev_delinq'] = data.curr_delinq.shift(1) ## needs attention here
-    #data['prev_delinq'] = data.groupby(level=0)['curr_delinq'].shift(1)
-    #print(sum(data.prev_delinq.isnull()))
+    y = y.apply(lambda x: 1 if x not in (0, 1) else 0)
+    # data['prev_delinq'] = data.curr_delinq.shift(1) ## needs attention here
+    # data['prev_delinq'] = data.groupby(level=0)['curr_delinq'].shift(1)
+    # print(sum(data.prev_delinq.isnull()))
     data.fillna(value=0, inplace=True, axis=1)
     data.drop(['curr_delinq'], axis=1, inplace=True)
     print(y.shape)
     ## how many classes are y?
     ## remove y from X
-    X = pd.get_dummies(data, columns=['first_time', 'occ_status', 'channel', 'ppm', 'fixed_rate',
-                                  'state', 'prop_type', 'loan_purpose', 'exceed_conform', 'repurchased'])
-    #y = label_binarize(y, classes=[0, 1, 2, 3]) ## do we really have to do this?
-    X[['credit_score','mi_perc','units','ocltv', 'odti', 'oupb', 'oltv', 'oint_rate','zip',
-       'curr_upb','loan_age','remain_months', 'curr_rate','curr_def_upb', 'ddlpi','mi_rec',
-       'non_mi_rec', 'exp', 'legal_costs','maint_exp','tax_insur', 'misc_exp', 'loss','mod_exp']] = \
-    scale(X[['credit_score','mi_perc','units','ocltv', 'odti', 'oupb', 'oltv', 'oint_rate','zip',
-       'curr_upb','loan_age','remain_months', 'curr_rate','curr_def_upb', 'ddlpi','mi_rec',
-       'non_mi_rec', 'exp', 'legal_costs','maint_exp','tax_insur', 'misc_exp', 'loss','mod_exp']], with_mean=False)
+
+    X = pd.get_dummies(data)
+    # X.net_proceeds = X.net_proceeds.apply(lambda x:0 if x == 'C' else x)
+    # y = label_binarize(y, classes=[0, 1, 2, 3]) ## do we really have to do this?
+    X[['credit_score', 'mi_perc', 'units', 'ocltv', 'oupb', 'oltv', 'oint_rate', 'zip',
+       'curr_upb', 'loan_age', 'remain_months', 'curr_rate', 'curr_def_upb', 'ddlpi', 'mi_rec',
+       'non_mi_rec', 'exp', 'legal_costs', 'maint_exp', 'tax_insur', 'misc_exp', 'loss', 'mod_exp']] = \
+        scale(X[['credit_score', 'mi_perc', 'units', 'ocltv', 'oupb', 'oltv', 'oint_rate', 'zip',
+                 'curr_upb', 'loan_age', 'remain_months', 'curr_rate', 'curr_def_upb', 'ddlpi', 'mi_rec',
+                 'non_mi_rec', 'exp', 'legal_costs', 'maint_exp', 'tax_insur', 'misc_exp', 'loss', 'mod_exp']],
+              with_mean=False)
     return X, y
 
 
-
-#all_data = get_all_data()
-#train, target = process_data(alldata)
-#print(all_data.shape)
-#all_data.to_csv('data_12_16.csv')
