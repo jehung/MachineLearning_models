59ee52c86a44e8e8a7aca8e0810b2032cdab87b8
jehung <jehung@me.com>
2017-09-04 02:03:32 -0700

    Refactor Decesion Tree for speed

--- dt_model.py
+++ dt_model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import time
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from imblearn.over_sampling import SMOTE, ADASYN
 from imblearn.combine import SMOTEENN
 from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
@@ -20,51 +21,38 @@ import utility
 from itertools import repeat
 
 
-d = {'train': [], 'cv set': [], 'test': []}
+d = {'train': None, 'cv set': None, 'test': None}
 
 
-def train_size_svc(X, y):
-    training_features, test_features, \
-    training_target, test_target, = train_test_split(X, y, test_size=0.33, random_state=778)
-    return training_features, test_features, training_target, test_target, d
-
-
-
-
-def fit_dt(train=None, target=None, size=0):
-    #for size in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+def train_size_dt(train, target, size=0):
     print('size', size)
     print('here')
-    training_features, test_features, training_target, test_target, d = train_size_svc(train, target)
+    training_features, test_features, \
+    training_target, test_target, = train_test_split(train, target, test_size=0.33, random_state=778)
     X_train, X_val, y_train, y_val = train_test_split(training_features, training_target, train_size=size)
-    #smote = SMOTE(ratio=1)
-    #X_train_res, y_train_res = smote.fit_sample(X_train, y_train)
     print('start')
     start_time = time.time()
     dt = DecisionTreeClassifier(class_weight='balanced')
-    dt.fit(X_train_res, y_train_res)
+    dt.fit(X_train, y_train)
     print('Decision Tree took', time.time() - start_time, 'to run')
-    print('process')
-    d['train'].append(f1_score(y_train, clf.predict(X_train), average='weighted'))
-    d['cv set'].append(f1_score(y_val, clf.predict(X_val), average='weighted'))
-    d['test'].append(f1_score(test_target, clf.predict(test_features), average='weighted'))
+    d['train'] = f1_score(y_train, dt.predict(X_train), average='weighted')
+    d['cv set'] = f1_score(y_val, dt.predict(X_val), average='weighted')
+    d['test'] = f1_score(test_target, dt.predict(test_features), average='weighted')
     print('end')
 
     return d
 
 
-
-def complexity_knn(X, y):
+def complexity_dt(X, y):
     #X_train, y_train, X_test, y_test = train_test_split(X,y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=778)
     #smote = SMOTE(ratio=1)
     #X_train_res, y_train_res = smote.fit_sample(X_train, y_train)
     print('Start Search')
-    knn= KNeighborsClassifier(class_weight='balanced')
-    pipe = Pipeline([('knn', knn)])
-    param_grid = {
-        'knn__n_neighbors': [3,7,11]}
-    grid_search = GridSearchCV(estimator=pipe, param_grid=param_grid, n_jobs=6, cv=3, scoring='neg_log_loss', verbose=5)
+    dt = DecisionTreeClassifier(class_weight='balanced')
+    pipe = Pipeline([('dt', dt)])
+    param_grid = {'dt__max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]}
+    grid_search = GridSearchCV(estimator=pipe, param_grid=param_grid, n_jobs=6, cv=5, scoring='neg_log_loss', verbose=5)
     grid_search.fit(X_train, y_train)
     clf = grid_search.best_estimator_
     print('clf', clf)
@@ -75,7 +63,7 @@ def complexity_knn(X, y):
     print(classification_report(y_test, y_pred, target_names=target_names))
     conf_mat = confusion_matrix(y_test, y_pred)
     plt.figure()
-    plot_confusion_matrix(conf_mat, classes=target_names,
+    utility.plot_confusion_matrix(conf_mat, classes=target_names,
                       title='Confusion matrix, without normalization')
     plt.show()
     return clf, clf.predict(X_train_res), y_pred
@@ -86,9 +74,7 @@ def complexity_knn(X, y):
 if  __name__== '__main__':
     all_data = get_all_data.get_all_data()
     train, target = get_all_data.process_data(all_data)
-    df = Parallel(n_jobs=6)(delayed(fit_dt)(train=train, target=target, size=size) for size in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-    merge_dict(df)
-    #pool = multiprocessing.Pool(processes=6)
-    #df = pool.starmap(fit_dt, zip(repeat(train, target), range(0.4,0.6,0.1)))
+    df = Parallel(n_jobs=6)(delayed(train_size_dt)(train=train, target=target, size=size) for size in np.arange(0.1, 1, 0.1))
+    df = utility.merge_dict(df)
     print(df)
-    #clf, score, mat = complexity_knn(train, target)
+    clf, score, mat = complexity_dt(train, target)
